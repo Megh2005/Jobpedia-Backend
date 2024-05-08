4b46bbf5529db50ad8aa66fc195999b8dbaf14eb
Megh Deb <20169629-pheonixflaunt@users.noreply.gitlab.com>
2024-05-08 20:32:15 +0530

    codes

---
+++ models/jobSchema.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+const jobSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, "Provide Job Title"],
+        minLength: [10, "Minimum 10 Characters"],
+        maxLength: [50, "Maximum 50 Characters"],
+    },
+    description: {
+        type: String,
+        required: [true, "Provide Job Description"],
+        minLength: [50, "Minimum 50 Characters"],
+        maxLength: [300, "Maximum 300 Characters"],
+    },
+    category: {
+        type: String,
+        required: [true, "Provide Job Category"],
+    },
+    country: {
+        type: String,
+        required: [true, "Provide Job Country"],
+    },
+    city: {
+        type: String,
+        required: [true, "Provide Job City"],
+    },
+    state: {
+        type: String,
+        required: [true, "Provide Job State"],
+    },
+    location: {
+        type: String,
+        required: [true, "Provide Job Address"],
+        minLength: [50, "Minimum 50 Characters"],
+        maxLength: [300, "Maximum 300 Characters"],
+    },
+    fixedSalary: {
+        type: Number,
+        minLength: [4, "Minimum 1K INR Should Be Given"],
+        maxLength: [6, "Maximum 100K INR Is Allowed"],
+    },
+    salaryFrom: {
+        type: Number,
+        minLength: [4, "Minimum 1K INR Should Be Given"],
+        maxLength: [6, "Maximum 100K INR Is Allowed"],
+    },
+    salaryTo: {
+        type: Number,
+        minLength: [5, "Minimum 10K INR Should Be Given"],
+        maxLength: [6, "Maximum 100K INR Is Allowed"],
+    },
+})
