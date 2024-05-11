7cff6398aab77203a179db32cb3f1d1fd665c505
Megh Deb <20169629-pheonixflaunt@users.noreply.gitlab.com>
2024-05-11 10:53:34 +0530

    codes

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getAllJobs, postJob } from '../controllers/jobController.js';
+import { getAllJobs, getMyJobs, postJob } from '../controllers/jobController.js';
 import { isAuthorised } from '../middlewares/authMiddleware.js';
 
 const jobRouter = express.Router();
 jobRouter.get("/alljobs", getAllJobs);
-jobRouter.post("/postjob", isAuthorised, postJob)
+jobRouter.post("/postjob", isAuthorised, postJob);
+jobRouter.post("/myjobs",isAuthorised,getMyJobs)
 
 export default jobRouter;
